3316ca68255a547201f5df3a706b9269ce7b8d39
Lenart <kovac.lenart@gmail.com>
2021-05-02 17:14:08 +0200

    added extra filters to get the core of some transfer names and make them easier to read

--- DataLoader.py
+++ DataLoader.py
@@ -15,10 +15,34 @@ class DataLoader:
                 row["BREME"] = float(row.get("BREME").replace(",", ".")) if not row.get("BREME") == '' else 0
                 row["DOBRO"] = float(row.get("DOBRO").replace(",", ".")) if not row.get("DOBRO") == '' else 0
                 row["NAMEN"] = row.get("NAMEN").strip()
+                row["NAMEN"] = row.get("NAMEN").upper()
 
-                #? collect all amazon tags under one name
-                if re.match("^AMZN Mktp|^Amazon", row["NAMEN"]):
+                #? process some data to limit it to only useful information
+                if re.match("AMZN|Amazon", row["NAMEN"]):
                     row["NAMEN"] = "AMAZON"
+                if re.match(".*LENKO.JAPAN", row["NAMEN"]):
+                    row["NAMEN"] = "SPOTIFY"
+                if re.match(".*LAST FM", row["NAMEN"]):
+                    row["NAMEN"] = "LASTFM"
+                if re.match(".*NETFLIX", row["NAMEN"]):
+                    row["NAMEN"] = "NETFLIX"
+                if re.match("APPLE.COM/BILL", row["NAMEN"]):
+                    row["NAMEN"] = "ICLOUD"
+                if re.match("WOLT", row["NAMEN"]):
+                    row["NAMEN"] = "WOLT"
+                if re.match(".*A1.*", row["NAMEN"]):
+                    row["NAMEN"] = "A1"
+                if re.match("HOFER", row["NAMEN"]):
+                    row["NAMEN"] = "HOFER"
+                if re.match("TELEKOM", row["NAMEN"]):
+                    row["NAMEN"] = "TELEKOM"
+                if re.match("MCDONALDS", row["NAMEN"]):
+                    row["NAMEN"] = "MCDONALDS"
 
                 dicts.append(row)
         return dicts
+
+
+if __name__ == "__main__":
+    data = DataLoader.monthlyData(2021, 2)
+    print([x["NAMEN"] for x in data])
