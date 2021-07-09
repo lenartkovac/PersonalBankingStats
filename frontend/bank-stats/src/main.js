83a3cf84ef96a0b95b255d6ef89f73766b7bf7f9
Lenart <kovac.lenart@gmail.com>
2021-07-09 14:37:54 +0200

    stylize the transaction tables

--- frontend/bank-stats/src/main.js
+++ frontend/bank-stats/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 
 const app = createApp(App)
