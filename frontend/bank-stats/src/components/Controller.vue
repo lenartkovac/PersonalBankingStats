547ecc28ebefd5c6c6d4e27552abfad3dda33e27
Lenart <kovac.lenart@gmail.com>
2021-08-05 12:32:07 +0200

    add currently viewed month to vuex store

--- frontend/bank-stats/src/components/Controller.vue
+++ frontend/bank-stats/src/components/Controller.vue
@@ -2,7 +2,7 @@
 	<div>
 		<div class="controller">
 			<span class="prev" @click="decMonth"><i class="fas fa-chevron-left"/></span>
-			<span>{{currentMonth}}</span>
+			<span>{{months[currentMonth - 1]}} {{currentMonth}}</span>
 			<span class="next" @click="incMonth"><i class="fas fa-chevron-right"/></span>
 		</div>
 		<div class="container">
@@ -14,6 +14,12 @@
 
 <script>
 import MonthStats from './MonthStats.vue'
+//import { useStore } from 'vuex'
+//import { 
+//	onMounted, 
+//	reactive, 
+//	toRefs 
+//} from 'vue'
 
 export default {
 	name: "Controller",
@@ -22,16 +28,36 @@ export default {
 	},
 	data() {
 		return {
-			currentMonth: 1
+			currentMonth: 2,
+			months: [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"
+			]
 		}
 	},
 	methods: {
 		incMonth() {
 			this.currentMonth = this.currentMonth == 12 ? 1 : this.currentMonth + 1;
+			this.$store.commit('changeMonth', this.currentMonth)
+			//console.log(this.$state)
 		},
 		decMonth() {
 			this.currentMonth = this.currentMonth ==  1 ? 12 : this.currentMonth - 1;
+			this.$store.commit('changeMonth', this.currentMonth)
 		}
+	},
+	mounted() {
+		this.currentMonth = this.$store.getters.getCurrMonth
 	}
 }
 
@@ -41,6 +67,9 @@ export default {
 .controller {
 	text-align: center;
 	font-size: 3em;
+	-webkit-user-select: none;  /* Chrome all / Safari all */
+	-moz-user-select: none;     /* Firefox all */
+	-ms-user-select: none;      /* IE 10+ */
 }
 
 .prev {
--- frontend/bank-stats/src/store/index.js
+++ frontend/bank-stats/src/store/index.js
@@ -1,17 +1,23 @@
 import { createStore } from 'vuex'
 
 const state = {
-	currTab: 'Incoming'
+	currTab: 'Incoming',
+	currMonth: 1
 }
 
 const mutations = {
 	changeTab(state, payload) {
 		state.currTab = payload
+	},
+	changeMonth(state, payload) {
+		state.currMonth = payload
+		console.log("state month changed to ", payload)
 	}
 }
 
 const getters = {
-	getCurrTab: state => state.currTab
+	getCurrTab: state => state.currTab,
+	getCurrMonth: state => state.currMonth
 }
 
 const store = createStore({
